2c49219aa70a0527d45cb53b0ac1a3fd80cf0eaf
Jason Tighe <tighe59@gmail.com>
2025-05-02 09:31:50 -0400

    unused state

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -3,7 +3,6 @@ import GameBoard from '../components/GameBoard/GameBoard';
 import ScorePanel from '../components/ScorePanel';
 
 export default function Settings() {
-  const [score, setScore] = useState(0);
   
   return (
     <div className="game-container">
