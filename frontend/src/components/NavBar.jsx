7bc3eff9ebd13f7f91d633d277ed1600faffe7b6
Jason Tighe <tighe59@gmail.com>
2025-05-14 13:50:30 -0400

    just adding some style to the nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,11 +2,21 @@ import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <nav>
-      <Link to="/">Start</Link>
-      <Link to='/game'>Game</Link>
-      <Link to="/stats">Stats</Link>
-      <Link to="/settings">Settings</Link>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-end space-x-4">
+        <Link to="/" className="text-white hover:text-gray-300">
+          Home
+        </Link>
+        <Link to="/game" className="text-white hover:text-gray-300">
+          Reset
+        </Link>
+        <Link to="/stats" className="text-white hover:text-gray-300">
+          Stats
+        </Link>
+        <Link to="/settings" className="text-white hover:text-gray-300">
+          Settings
+        </Link>
+      </div>
     </nav>
   )
 }
